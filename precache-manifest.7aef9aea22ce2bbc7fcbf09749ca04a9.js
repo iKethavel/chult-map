892991fa287d40527d46d9adc4aae6fc23957e3d
Rostyslav <blackswordgc@gmail.com>
2020-03-25 17:15:15 +0200

    Updates

---
+++ precache-manifest.7aef9aea22ce2bbc7fcbf09749ca04a9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3cf88fd60ff849ad73e825daf34a0f2",
+    "url": "/chult-map/index.html"
+  },
+  {
+    "revision": "056ba8bed12ee3d581f5",
+    "url": "/chult-map/static/css/2.62cbf31a.chunk.css"
+  },
+  {
+    "revision": "58dea57978fd6f89d303",
+    "url": "/chult-map/static/css/main.32aa61ed.chunk.css"
+  },
+  {
+    "revision": "056ba8bed12ee3d581f5",
+    "url": "/chult-map/static/js/2.dc2d05c4.chunk.js"
+  },
+  {
+    "revision": "c1b26eb56f0d464d8c15397856fe014f",
+    "url": "/chult-map/static/js/2.dc2d05c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58dea57978fd6f89d303",
+    "url": "/chult-map/static/js/main.4c5deaa0.chunk.js"
+  },
+  {
+    "revision": "05d1e6fab2604bfcdb33",
+    "url": "/chult-map/static/js/runtime-main.75b6c161.js"
+  },
+  {
+    "revision": "42dc2325e509f2317f414378f377e6a0",
+    "url": "/chult-map/static/media/map.42dc2325.jpg"
+  }
+]);
